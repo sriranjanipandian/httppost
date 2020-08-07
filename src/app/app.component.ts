c840b9a318fcd946ea18d278d6cc4857cdb42efc
sriranjanipandian <sriranjani207@gmail.com>
2020-08-07 15:48:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { Person } from './person';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'httpost';
+export class AppComponent implements OnInit {
+ 
+  title = 'httpGet Example';
+  people:Person[];
+  person = new Person();
+  
+  constructor(private apiService:ApiService) {}
+ 
+  ngOnInit() {
+    this.refreshPeople()
+  }
+ 
+  refreshPeople() {
+    this.apiService.getPeople()
+      .subscribe(data => {
+        console.log(data)
+        this.people=data;
+      })      
+ 
+  }
+ 
+  addPerson() {
+    this.apiService.addPerson(this.person)
+      .subscribe(data => {
+        console.log(data)
+        this.refreshPeople();
+      })      
+  }
+ 
 }
